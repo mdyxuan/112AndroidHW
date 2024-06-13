58bfc25122a25aad219105e95d21af62c86bc16e
mdyxuan <emily450033@gmail.com>
2024-06-14 00:40:55 +0800

    2024-06-14

--- MenuHW/app/src/main/java/com/example/menuhw/MainActivity.java
+++ MenuHW/app/src/main/java/com/example/menuhw/MainActivity.java
@@ -16,49 +16,65 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner menu;
-    private ListView Meal,Extra,Drink;
-    private String[] MealSet,ExtraSet,DrinkSet;
+    private Spinner spinnerMainCourse, spinnerSideDish, spinnerDrinks;
+    private TextView tvOrderSummary;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MealSet = getResources().getStringArray(R.array.meal);
-        ExtraSet = getResources().getStringArray(R.array.extra);
-        DrinkSet = getResources().getStringArray(R.array.drink);
-        menu = (Spinner) findViewById(R.id.menu);
 
-        menu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
-            {
-                if(position == 0)
-                {
-                    ArrayAdapter<String> a = new ArrayAdapter<>(this,
-                            android.R.layout.simple_spinner_dropdown_item, MealSet);
-                    Meal = (ListView) findViewById(R.id.ListView);
-                    Meal.setAdapter(a);
-                    Meal.setOnItemClickListener(this);
-                }
-            }
+        // 初始化控件
+        spinnerMainCourse = findViewById(R.id.spinner_main_course);
+        spinnerSideDish = findViewById(R.id.spinner_side_dish);
+        spinnerDrinks = findViewById(R.id.spinner_drinks);
+        tvOrderSummary = findViewById(R.id.tv_order_summary);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+        // 设置 Spinner 适配器
+        ArrayAdapter<CharSequence> mainCourseAdapter = ArrayAdapter.createFromResource(this,
+                R.array.main_course_array, android.R.layout.simple_spinner_item);
+        mainCourseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMainCourse.setAdapter(mainCourseAdapter);
 
-            }
-        });
+        ArrayAdapter<CharSequence> sideDishAdapter = ArrayAdapter.createFromResource(this,
+                R.array.side_dish_array, android.R.layout.simple_spinner_item);
+        sideDishAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSideDish.setAdapter(sideDishAdapter);
+
+        ArrayAdapter<CharSequence> drinksAdapter = ArrayAdapter.createFromResource(this,
+                R.array.drinks_array, android.R.layout.simple_spinner_item);
+        drinksAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDrinks.setAdapter(drinksAdapter);
     }
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 处理菜单项选择
+        if (item.getItemId() == R.id.toSend)
+        {
+            String mainCourse = spinnerMainCourse.getSelectedItem().toString();
+            String sideDish = spinnerSideDish.getSelectedItem().toString();
+            String drink = spinnerDrinks.getSelectedItem().toString();
 
+            String orderSummary = "主餐: " + mainCourse + "\n附餐: " + sideDish + "\n饮料: " + drink;
+            tvOrderSummary.setText(orderSummary);
+            return true;
+        }
+        else if(item.getItemId() == R.id.toCancel)
+        {
+            spinnerMainCourse.setSelection(0);
+            spinnerSideDish.setSelection(0);
+            spinnerDrinks.setSelection(0);
+            tvOrderSummary.setText("");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
